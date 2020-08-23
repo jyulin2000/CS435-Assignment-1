d2b73f76b8b6107e0fc028d5a386fe9aa68c0484
jyulin2000 <jyulin2000@gmail.com>
2020-08-22 19:45:47 -0500

    Implemented leaf-node parsing

--- Main.java
+++ Main.java
@@ -1,16 +1,39 @@
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.List;
 
 public class Main {
 	public static void main(String[] args) {
-		File inputDir = new File(args[0]);
+		File inputDir = new File(args[0] + File.separator);
+		File outputDir = new File(Paths.get(".").toAbsolutePath().normalize().toString() + File.separator + "output" + File.separator);
+		outputDir.mkdir();
 		
-		// Find every xml file, parse and annotate corresgonding image appropriately
+		// Find every xml file, parse and annotate corresponding image appropriately
 		// We assume there is a matching *.png to every *.xml
 		for (File file : inputDir.listFiles()) {
 			if (file.getName().endsWith(".xml")) {
 				String name = file.getName();
-				
-				System.out.println(name.substring(0, name.length() - 4));
+				name = name.substring(0, name.length() - 4);
+				File xmlFile = new File(inputDir.toString() + File.separator + name + ".xml");
+				XMLParser parser = new XMLParser(xmlFile);
+				if (parser.isValidXML()) {
+					List<int[]> boundaries = parser.getBoundaries();
+					
+					File imgFile = new File(inputDir.toString() + File.separator + name + ".png");
+					ImageAnnotator img = new ImageAnnotator(imgFile);
+					
+					for (int[] bound : boundaries) {
+						// System.out.println("Bounds: " + bound[0] + ", " + bound[1] + ", "+ bound[2] + ", "+ bound[3]);
+						img.outlineRegion(bound[0], bound[1], bound[2], bound[3]);
+					}
+					
+					File outputFile = new File(outputDir.toString() + File.separator + name + "-ANNOTATED.png");
+					img.writeToNewFile(outputFile);
+					System.out.println(outputFile.getName() + " created.");
+				} else {
+					System.out.println(xmlFile.getName() + " is not a valid XML document. Skipping to next file.");
+				}
 			}
 		}
 	}
--- XMLParser.java
+++ XMLParser.java
@@ -1,7 +1,75 @@
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
+
+import javax.xml.XMLConstants;
+import javax.xml.parsers.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 public class XMLParser {
+	private DocumentBuilderFactory factory;
+	private DocumentBuilder builder;
+	private Document document;
+	private File file;
+	private boolean isValid;
+	List<int[]> boundaries;
 	
-	public XMLParser(String path) {
-		System.out.println("fuckle");
+	public XMLParser(File file) {
+		this.file = file;
+		factory = DocumentBuilderFactory.newInstance();
+		try {
+			builder = factory.newDocumentBuilder();
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		}
+		isValid = true;
+		try {
+			document = builder.parse(file);
+		} catch (SAXException e) {
+			isValid = false;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public boolean isValidXML() {
+		return isValid;
+	}
+	
+	public List<int[]> getBoundaries() {
+		boundaries = new LinkedList<int[]>();
+		findLeaves(document.getDocumentElement());
+		return boundaries;
+	}
+	
+	// Recursively finds leaf nodes and returns
+	// 
+	private void findLeaves(Node node) {
+		if (node.hasChildNodes()) {
+			NodeList children = node.getChildNodes();
+			for (int i = 0; i < children.getLength(); i++) {
+				findLeaves(children.item(i));
+			}
+		}
+		else {
+			if (node.getAttributes() != null) {
+				String[] strings = ((Element) node).getAttribute("bounds").split("[\\[\\], ]");
+
+				int[] bound = new int[4];
+				bound[0] = Integer.parseInt(strings[1]);
+				bound[1] = Integer.parseInt(strings[2]);
+				bound[2] = Integer.parseInt(strings[4]);
+				bound[3] = Integer.parseInt(strings[5]);
+				
+				boundaries.add(bound);
+			}
+		}
 	}
 }
